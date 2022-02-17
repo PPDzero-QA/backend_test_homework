c9b1a3d862a1d42b8f89f7d8ee27d4ed0c7189f7
Ruslan Sabirzyanov <dota.buy@yandex.ru>
2022-02-17 18:06:13 +0300

    Test

--- program.py
+++ program.py
@@ -1,1 +1,29 @@
-print('Я домашка')
+"""Импорт sqrt из math."""
+from math import sqrt
+
+message = ('Добро пожаловать в самую лучшую программу для вычисления '
+           'квадратного корня из заданного числа')
+
+
+def calculate_square_root(number):
+    """Вычисляет квадратный корень."""
+    return sqrt(number)
+
+
+def calc(your_number):
+    """
+    Выводит текст с вычисленым квадратным корнем.
+
+    Если значение <=0, выводится только message.
+    """
+    root = calculate_square_root(your_number)
+    if your_number <= 0:
+        return
+    text = ('Мы вычислили корень квадратный из введенного вами числа.'
+            ' Это будет: ')
+
+    print(f"{text}{root}")
+
+
+print(message)
+calc(25.5)
